6e138afa1e7436e7a62012176c2081de4359e1eb
Domagoj Vukovic <off.vukovic@gmail.com>
2020-02-25 23:12:30 +0100

    - Made marker display video on trigger

--- js/ARCarDemo.js
+++ js/ARCarDemo.js
@@ -1,77 +1,33 @@
 'use strict';
 
 import React from 'react';
-import {
-    Viro3DObject,
-    ViroAnimations,
-    ViroARImageMarker,
-    ViroARScene,
-    ViroARTrackingTargets,
-    ViroLightingEnvironment,
-    ViroMaterials,
-} from 'react-viro';
+import { ViroARImageMarker, ViroARScene, ViroARTrackingTargets, ViroVideo, } from 'react-viro';
 
-class ARCarDemo extends React.Component {
-    state = {
-        texture: "white",
-        playAnim: false,
-        animateCar: false,
-        tapWhite: false,
-        tapBlue: false,
-        tapGrey: false,
-        tapRed: false,
-        tapYellow: false,
-    };
-
-    handleTargetFound = () => {
-        this.setState({ animateCar: true })
-    };
-
-    render() {
-        return (
-            <ViroARScene>
-                <ViroLightingEnvironment source={require('./res/tesla/garage_1k.hdr')} />
-                <ViroARImageMarker
-                    target={"logo"}
-                    onAnchorFound={this.handleTargetFound}
-                    pauseUpdates={this.state.pauseUpdates}
-                >
-                    <Viro3DObject
-                        scale={[0, 0, 0]}
-                        source={require('./res/tesla/object_car.obj')}
-                        resources={[require('./res/tesla/object_car.mtl')]}
-                        type="OBJ"
-                        materials={this.state.texture}
-                        animation={{ name: "scaleCar", run: this.state.animateCar, }}
-                    />
-                </ViroARImageMarker>
-            </ViroARScene>
-        );
-    }
-}
-
-ViroMaterials.createMaterials({
-    white: {
-        lightingModel: "PBR",
-        diffuseTexture: require('./res/tesla/object_car_main_Base_Color.png'),
-        metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
-        roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
-    },
-});
+export const ARCarDemo = () => {
+    return (
+        <ViroARScene>
+            <ViroARImageMarker target={"logo"}>
+                <ViroVideo
+                    source={require("./res/video.mp4")}
+                    volume={1}
+                    loop={true}
+                    scale={[0.1, 0.1, 0.1]}
+                    position={[0, 0, 0]}
+                    rotation={[-80, 0, 0]}
+                    width={2}
+                    height={2}
+                />
+            </ViroARImageMarker>
+        </ViroARScene>
+    );
+};
 
 ViroARTrackingTargets.createTargets({
     logo: {
         source: require('./res/shirt.png'),
         orientation: "Up",
-        physicalWidth: 0.165
+        physicalWidth: 0.25
     }
-});
-
-ViroAnimations.registerAnimations({
-    scaleCar: {
-        properties: { scaleX: .09, scaleY: .09, scaleZ: .09, },
-        duration: 500, easing: "bounce"
-    },
 });
 
 export default ARCarDemo
